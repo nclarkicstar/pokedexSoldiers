0f9620eb3d6c12db6775d476a8d59456ba9ec505
Nathan Clark <nclark@icstars.org>
2023-11-21 21:04:00 -0600

    init

--- scripts.js
+++ scripts.js
@@ -1,43 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Event listener for each grid image
   document.querySelectorAll(".gridImage").forEach((item) => {
-    item.addEventListener("click", () => {
-      const pokemonName = item.querySelector(".pokemonName").textContent;
-      const pokemonImage = item.querySelector("img").src;
-      const overlay = document.getElementById("pokemonOverlay");
-      const overlayContent = document.getElementById("overlayContent");
-
-      // Set the content for the overlay
-      document.getElementById("overlayName").textContent = pokemonName;
-      document.getElementById("overlayImage").src = pokemonImage;
-
-      // Show the overlay and content
-      overlay.style.display = "flex";
-      setTimeout(() => {
-        overlay.style.opacity = "1";
-        overlayContent.style.transform = "translateY(0)";
-      }, 10);
-    });
+    item.addEventListener("click", openOverlay);
   });
 
-  // Event listener for the close button
-  document.getElementById("closeOverlay").addEventListener("click", () => {
-    const overlay = document.getElementById("pokemonOverlay");
-    const overlayContent = document.getElementById("overlayContent");
-
-    // Start the fade-out and slide-down animations
-    overlay.style.opacity = "0";
-    overlayContent.style.transform = "translateY(100%)";
-
-    // Wait for the fade-out animation to complete before hiding the overlay
-    overlay.addEventListener(
-      "transitionend",
-      (event) => {
-        if (event.propertyName === "opacity") {
-          overlay.style.display = "none";
-        }
-      },
-      { once: true }
-    );
-  });
+  const closeOverlayButton = document.getElementById("closeOverlay");
+  closeOverlayButton.addEventListener("click", closeOverlay);
+  closeOverlayButton.addEventListener("touchend", closeOverlay); // Fallback for touch devices
 });
+
+function openOverlay() {
+  const pokemonName = this.querySelector(".pokemonName").textContent;
+  const pokemonImage = this.querySelector("img").src;
+  const overlay = document.getElementById("pokemonOverlay");
+  const overlayContent = document.getElementById("overlayContent");
+
+  document.getElementById("overlayName").textContent = pokemonName;
+  document.getElementById("overlayImage").src = pokemonImage;
+
+  overlay.style.display = "flex";
+  setTimeout(() => {
+    overlay.style.opacity = "1";
+    overlayContent.style.transform = "translateY(0)";
+  }, 10);
+}
+
+function closeOverlay() {
+  const overlay = document.getElementById("pokemonOverlay");
+  const overlayContent = document.getElementById("overlayContent");
+
+  overlay.style.opacity = "0";
+  overlayContent.style.transform = "translateY(100%)";
+
+  setTimeout(() => {
+    overlay.style.display = "none";
+  }, 500); // Match this with your longest transition duration
+}
