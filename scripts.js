288a4ac9cc8d5b59474d50c011637dc4e052a680
Nathan Clark <nclark@icstars.org>
2023-11-21 20:47:26 -0600

    init

--- scripts.js
+++ scripts.js
@@ -5,17 +5,21 @@ document.querySelectorAll(".gridImage").forEach((item) => {
     const overlay = document.getElementById("pokemonOverlay");
     const overlayContent = document.getElementById("overlayContent");
 
+    // Set the content for the overlay
     document.getElementById("overlayName").textContent = pokemonName;
     document.getElementById("overlayImage").src = pokemonImage;
 
-    // Reset any existing animations
+    // Remove the classes that hide the overlay
     overlay.classList.remove("overlay-fade-out");
     overlayContent.classList.remove("overlay-slide-out");
 
-    // Apply styles to make the overlay visible and start animations
+    // Reset the display and opacity to show the overlay
     overlay.style.display = "flex";
-    overlay.style.opacity = "1";
-    overlayContent.style.transform = "translateY(0)";
+    setTimeout(() => {
+      // Timeout to apply the display property
+      overlay.style.opacity = "1";
+      overlayContent.style.transform = "translateY(0)";
+    }, 10); // Short timeout before the CSS transitions
   });
 });
 
@@ -24,14 +28,15 @@ document.getElementById("closeOverlay").addEventListener("click", () => {
   const overlayContent = document.getElementById("overlayContent");
 
   // Start the fade out and slide down animations
-  overlay.classList.add("overlay-fade-out");
-  overlayContent.classList.add("overlay-slide-out");
+  overlay.style.opacity = "0";
+  overlayContent.style.transform = "translateY(100%)";
 
-  // Set a timeout to match the animation duration
-  setTimeout(() => {
-    overlay.style.display = "none";
-    // Reset styles after animation
-    overlay.style.opacity = "0";
-    overlayContent.style.transform = "translateY(100%)";
-  }, 500); // This duration should match the longest animation time
+  // Use the 'transitionend' event to listen for when the animation ends
+  overlay.addEventListener(
+    "transitionend",
+    () => {
+      overlay.style.display = "none";
+    },
+    { once: true }
+  ); // Use the { once: true } option so the event is only handled once
 });
