077d5d76ba48870edd42225f8d0daf765af9c29a
Nathan Clark <nclark@icstars.org>
2023-11-21 22:04:38 -0600

    init

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Add click event listeners to each grid image
   document.querySelectorAll(".gridImage").forEach((item) => {
     item.addEventListener("click", openOverlay);
   });
 
+  // Event listeners for closing the overlay
   const closeOverlayButton = document.getElementById("closeOverlay");
   closeOverlayButton.addEventListener("click", closeOverlay);
   closeOverlayButton.addEventListener("touchend", closeOverlay); // Fallback for touch devices
@@ -12,10 +14,51 @@ function openOverlay() {
   const pokemonName = this.querySelector(".pokemonName").textContent;
   const pokemonImage = this.querySelector("img").src;
   const overlay = document.getElementById("pokemonOverlay");
-  const overlayContent = document.getElementById("overlayContent");
 
+  // Set the content for the overlay
   document.getElementById("overlayName").textContent = pokemonName;
   document.getElementById("overlayImage").src = pokemonImage;
+  const overlayText = document.getElementById("overlayText");
+  if (!overlayText) {
+    console.error("Element with ID 'overlayText' not found.");
+    return;
+  }
+
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const pokemonData = data[pokemonName];
+      if (pokemonData) {
+        overlayText.innerHTML = `
+            <h3>Type: ${pokemonData.type}</h3>
+            <h4>Abilities: ${pokemonData.abilities.join(", ")}</h4>
+            <p>${pokemonData.description}</p>
+            <div class="skill-bars">
+              ${createSkillBar(
+                pokemonData.skilllevel1Name,
+                pokemonData.skilllevel1Level
+              )}
+              ${createSkillBar(
+                pokemonData.skilllevel2Name,
+                pokemonData.skilllevel2Level
+              )}
+              ${createSkillBar(
+                pokemonData.skilllevel3Name,
+                pokemonData.skilllevel3Level
+              )}
+            </div>
+            <h4>Strengths:</h4>
+            <ul>${pokemonData.strengths
+              .map((strength) => `<li>${strength}</li>`)
+              .join("")}</ul>
+            <h4>Weaknesses:</h4>
+            <ul>${pokemonData.weaknesses
+              .map((weakness) => `<li>${weakness}</li>`)
+              .join("")}</ul>
+          `;
+      }
+    })
+    .catch((error) => console.error("Error:", error));
 
   overlay.style.display = "flex";
   setTimeout(() => {
@@ -24,10 +67,24 @@ function openOverlay() {
   }, 10);
 }
 
+function createSkillBar(skillName, skillLevel) {
+  return `
+      <div class="skill">
+        <span>${skillName}: </span>
+        <div class="skill-bar">
+          <div class="skill-fill" style="width: ${
+            (skillLevel * 100) / 15
+          }%;"></div>
+        </div>
+      </div>
+    `;
+}
+
 function closeOverlay() {
   const overlay = document.getElementById("pokemonOverlay");
   const overlayContent = document.getElementById("overlayContent");
 
+  // Hide the overlay
   overlay.style.opacity = "0";
   overlayContent.style.transform = "translateY(100%)";
 
