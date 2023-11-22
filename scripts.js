dd865b68c9f0cf13f9ad9d83d62dc71583648b2f
Nathan Clark <nclark@icstars.org>
2023-11-21 20:56:29 -0600

    init

--- scripts.js
+++ scripts.js
@@ -1,42 +1,43 @@
-document.querySelectorAll(".gridImage").forEach((item) => {
-  item.addEventListener("click", (event) => {
-    const pokemonName = item.querySelector(".pokemonName").textContent;
-    const pokemonImage = item.querySelector("img").src;
+document.addEventListener("DOMContentLoaded", () => {
+  // Event listener for each grid image
+  document.querySelectorAll(".gridImage").forEach((item) => {
+    item.addEventListener("click", () => {
+      const pokemonName = item.querySelector(".pokemonName").textContent;
+      const pokemonImage = item.querySelector("img").src;
+      const overlay = document.getElementById("pokemonOverlay");
+      const overlayContent = document.getElementById("overlayContent");
+
+      // Set the content for the overlay
+      document.getElementById("overlayName").textContent = pokemonName;
+      document.getElementById("overlayImage").src = pokemonImage;
+
+      // Show the overlay and content
+      overlay.style.display = "flex";
+      setTimeout(() => {
+        overlay.style.opacity = "1";
+        overlayContent.style.transform = "translateY(0)";
+      }, 10);
+    });
+  });
+
+  // Event listener for the close button
+  document.getElementById("closeOverlay").addEventListener("click", () => {
     const overlay = document.getElementById("pokemonOverlay");
     const overlayContent = document.getElementById("overlayContent");
 
-    // Set the content for the overlay
-    document.getElementById("overlayName").textContent = pokemonName;
-    document.getElementById("overlayImage").src = pokemonImage;
+    // Start the fade-out and slide-down animations
+    overlay.style.opacity = "0";
+    overlayContent.style.transform = "translateY(100%)";
 
-    // Remove the classes that hide the overlay
-    overlay.classList.remove("overlay-fade-out");
-    overlayContent.classList.remove("overlay-slide-out");
-
-    // Reset the display and opacity to show the overlay
-    overlay.style.display = "flex";
-    setTimeout(() => {
-      // Timeout to apply the display property
-      overlay.style.opacity = "1";
-      overlayContent.style.transform = "translateY(0)";
-    }, 10); // Short timeout before the CSS transitions
+    // Wait for the fade-out animation to complete before hiding the overlay
+    overlay.addEventListener(
+      "transitionend",
+      (event) => {
+        if (event.propertyName === "opacity") {
+          overlay.style.display = "none";
+        }
+      },
+      { once: true }
+    );
   });
-});
-
-document.getElementById("closeOverlay").addEventListener("click", () => {
-  const overlay = document.getElementById("pokemonOverlay");
-  const overlayContent = document.getElementById("overlayContent");
-
-  // Start the fade out and slide down animations
-  overlay.style.opacity = "0";
-  overlayContent.style.transform = "translateY(100%)";
-
-  // Use the 'transitionend' event to listen for when the animation ends
-  overlay.addEventListener(
-    "transitionend",
-    () => {
-      overlay.style.display = "none";
-    },
-    { once: true }
-  ); // Use the { once: true } option so the event is only handled once
 });
